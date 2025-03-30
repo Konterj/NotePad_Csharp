6598c0245324d975e08e687c235f2215803e75de
Kontertj <128936128+Konterj@users.noreply.github.com>
2025-03-30 18:45:27 +0300

    Ready_This_Project

---
+++ NotePad/NotePad/Control_Size_Font.xaml.cs
@@ -0,0 +1,33 @@
+﻿// В Control_Size_Font.xaml.cs
+using System.Windows;
+
+namespace NotePad
+{
+    /// <summary>
+    /// Логика взаимодействия для Control_Size_Font.xaml
+    /// </summary>
+    public partial class Control_Size_Font : Window
+    {
+        private MainWindow _mainWindow;
+
+        public Control_Size_Font(MainWindow mainWindow) 
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+
+        private void CLick_FontSize_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TxtSize.Text, out double fontSize)) 
+            {
+                _mainWindow.txtEditor.FontSize = fontSize;
+                this.Close(); 
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректное числовое значение для размера шрифта.", "Ошибка");
+            }
+        }
+
+    }
+}
--- NotePad/NotePad/MainWindow.xaml.cs
+++ NotePad/NotePad/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace NotePad
 {
@@ -7,6 +10,7 @@ namespace NotePad
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string currentFilePath;
         public MainWindow()
         {
             InitializeComponent();
@@ -20,31 +24,95 @@ namespace NotePad
 
         private void menuFileExit_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!string.IsNullOrEmpty(currentFilePath)) 
+            {
+                menuFileSave_Click(sender, e);
+            }
+            else
+            {
+                menuFileSaveAs_Click(sender, e);
+            }
         }
 
         private void menuFileSaveAs_Click(object sender, RoutedEventArgs e)
         {
-
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "текстовые файлы (*.txt*)|*.txt|Все файлы (*.*)|*.*)";
+            if(saveFileDialog.ShowDialog() == true)
+            {
+                currentFilePath = saveFileDialog.FileName;
+                File.WriteAllText(currentFilePath, txtEditor.Text);
+                this.Title = $"Мой блокнот - {Path.GetFileName(currentFilePath)}";
+            }
         }
 
         private void menuFileSave_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!string.IsNullOrEmpty(currentFilePath)) 
+            {
+                File.WriteAllText(currentFilePath, txtEditor.Text);
+            }
+            else
+            {
+                menuFileSaveAs_Click(sender, e);
+            }
         }
 
         private void menuFileOpen_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                currentFilePath = openFileDialog.FileName;
+                txtEditor.Text = File.ReadAllText(currentFilePath);
+                this.Title = $"Мой Блокнот - {Path.GetFileName(currentFilePath)}";
+            }
         }
 
         private void menuFileNew_Click(object sender, RoutedEventArgs e)
         {
+            if (!string.IsNullOrEmpty(txtEditor.Text) && !string.IsNullOrEmpty(currentFilePath) && File.ReadAllText(currentFilePath) != txtEditor.Text)
+            {
+                MessageBoxResult result = MessageBox.Show("Сохранить изменения перед созданием нового файла?", "Предупреждение", MessageBoxButton.YesNoCancel);
+                if (result == MessageBoxResult.Yes)
+                {
+                    menuFileSave_Click(sender, e); 
+                }
+                else if (result == MessageBoxResult.Cancel)
+                {
+                    return;
+                }
+            }
+            else if (!string.IsNullOrEmpty(txtEditor.Text) && string.IsNullOrEmpty(currentFilePath))
+            {
+                MessageBoxResult result = MessageBox.Show("Сохранить изменения перед созданием нового файла?", "Предупреждение", MessageBoxButton.YesNoCancel);
+                if (result == MessageBoxResult.Yes)
+                {
+                    menuFileSaveAs_Click(sender, e);
+                }
+                else if (result == MessageBoxResult.Cancel)
+                {
+                    return;
+                }
+            }
+
+            txtEditor.Clear();
+
+            currentFilePath = null;
+
+            this.Title = "Мой Блокнот";
         }
 
-        private void txtEditor_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        private void txtEditor_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void Control_Size_Text_Click(object sender, RoutedEventArgs e)
+        {
+            Control_Size_Font fontSizeWindow = new Control_Size_Font(this); // Передаем ссылку на текущее окно MainWindow
+            fontSizeWindow.ShowDialog(); // Или fontSizeWindow.Show(); если не хочешь модальное окно
         }
     }
 }
