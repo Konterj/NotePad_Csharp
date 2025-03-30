f7dc0295e082760975772e8a21e4d4ac197c1d56
Kontertj <128936128+Konterj@users.noreply.github.com>
2025-03-30 17:48:17 +0300

    Update UI_Bloknot

--- NotePad/NotePad/MainWindow.xaml.cs
+++ NotePad/NotePad/MainWindow.xaml.cs
@@ -14,7 +14,8 @@ namespace NotePad
 
         private void menuHelpInfo_Click(object sender, RoutedEventArgs e)
         {
-
+            InfoWindow info = new InfoWindow();
+            info.ShowDialog();
         }
 
         private void menuFileExit_Click(object sender, RoutedEventArgs e)
@@ -38,6 +39,10 @@ namespace NotePad
         }
 
         private void menuFileNew_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void txtEditor_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
 
         }
